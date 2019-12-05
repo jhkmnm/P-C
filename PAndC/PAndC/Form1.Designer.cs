b2e1af5e56ffb299e5d8edfa493625c5bbea5958
sk <shenk@yhglobal.com>
2019-12-05 18:01:13 +0800

    1205

--- PAndC/PAndC/Form1.Designer.cs
+++ PAndC/PAndC/Form1.Designer.cs
@@ -28,21 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.btnClearA = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnClearB = new System.Windows.Forms.Button();
+            this.txtNums = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.lbxLeft = new System.Windows.Forms.ListBox();
+            this.lblSelected = new System.Windows.Forms.ListBox();
+            this.lbxOutput = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnClearA
+            // 
+            this.btnClearA.Location = new System.Drawing.Point(564, 9);
+            this.btnClearA.Name = "btnClearA";
+            this.btnClearA.Size = new System.Drawing.Size(75, 26);
+            this.btnClearA.TabIndex = 0;
+            this.btnClearA.Text = "清空";
+            this.btnClearA.UseVisualStyleBackColor = true;
+            this.btnClearA.Click += new System.EventHandler(this.btnClearA_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1102, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "输出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnClearB
+            // 
+            this.btnClearB.Location = new System.Drawing.Point(964, 9);
+            this.btnClearB.Name = "btnClearB";
+            this.btnClearB.Size = new System.Drawing.Size(75, 26);
+            this.btnClearB.TabIndex = 2;
+            this.btnClearB.Text = "清空";
+            this.btnClearB.UseVisualStyleBackColor = true;
+            this.btnClearB.Click += new System.EventHandler(this.btnClearB_Click);
+            // 
+            // txtNums
+            // 
+            this.txtNums.Location = new System.Drawing.Point(12, 12);
+            this.txtNums.Name = "txtNums";
+            this.txtNums.Size = new System.Drawing.Size(239, 25);
+            this.txtNums.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(257, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(67, 26);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "添加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(330, 12);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(67, 26);
+            this.btnImport.TabIndex = 5;
+            this.btnImport.Text = "导入";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // lbxLeft
+            // 
+            this.lbxLeft.ContextMenuStrip = this.contextMenuStrip1;
+            this.lbxLeft.FormattingEnabled = true;
+            this.lbxLeft.ItemHeight = 15;
+            this.lbxLeft.Location = new System.Drawing.Point(12, 53);
+            this.lbxLeft.Name = "lbxLeft";
+            this.lbxLeft.Size = new System.Drawing.Size(385, 589);
+            this.lbxLeft.TabIndex = 6;
+            this.lbxLeft.SelectedIndexChanged += new System.EventHandler(this.lbxLeft_SelectedIndexChanged);
+            // 
+            // lblSelected
+            // 
+            this.lblSelected.FormattingEnabled = true;
+            this.lblSelected.ItemHeight = 15;
+            this.lblSelected.Location = new System.Drawing.Point(403, 53);
+            this.lblSelected.Name = "lblSelected";
+            this.lblSelected.Size = new System.Drawing.Size(385, 589);
+            this.lblSelected.TabIndex = 7;
+            // 
+            // lbxOutput
+            // 
+            this.lbxOutput.FormattingEnabled = true;
+            this.lbxOutput.ItemHeight = 15;
+            this.lbxOutput.Location = new System.Drawing.Point(794, 53);
+            this.lbxOutput.Name = "lbxOutput";
+            this.lbxOutput.Size = new System.Drawing.Size(385, 589);
+            this.lbxOutput.TabIndex = 8;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.删除ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(109, 28);
+            // 
+            // 删除ToolStripMenuItem
+            // 
+            this.删除ToolStripMenuItem.Name = "删除ToolStripMenuItem";
+            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(108, 24);
+            this.删除ToolStripMenuItem.Text = "删除";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1189, 653);
+            this.Controls.Add(this.lbxOutput);
+            this.Controls.Add(this.lblSelected);
+            this.Controls.Add(this.lbxLeft);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNums);
+            this.Controls.Add(this.btnClearB);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnClearA);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnClearA;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnClearB;
+        private System.Windows.Forms.TextBox txtNums;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.ListBox lbxLeft;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 删除ToolStripMenuItem;
+        private System.Windows.Forms.ListBox lblSelected;
+        private System.Windows.Forms.ListBox lbxOutput;
     }
 }
 
--- PAndC/PAndC/Form1.cs
+++ PAndC/PAndC/Form1.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +16,7 @@ namespace PAndC
     public partial class Form1 : Form
     {
         List<List<int>> leftList = null;
+        List<List<int>> selectedList = null;
 
         public Form1()
         {
@@ -22,57 +25,185 @@ namespace PAndC
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //左侧数据
-            leftList = BuildLeftList();
-            var leftListCopy = leftList;
+            
+        }
+        
+        /// <summary>
+        /// 将文本转成数组，数字之间使用空格分隔
+        /// </summary>
+        /// <param name="lineText">单行文本</param>
+        private bool BuildLeftList(string lineText)
+        {
+            var textSplit = lineText.Split(' ');
+            var leftTemp = new List<int>();
 
-            //选择后的数据
-            List<int[]> selectedList = new List<int[]>();
-            selectedList.Add(SelectLeftList(0));
-            //selectedList.Add(SelectLeftList(3));
+            foreach(var str in textSplit)
+            {
+                int i = 0;
+                if(int.TryParse(str, out i))
+                {
+                    leftTemp.Add(i);
+                }
+            }
+
+            if(leftTemp.Count != 10)
+            {
+                MessageBox.Show("输入的数字有问题，确认一组是否是10个数字，数字之间使用空格分隔");
+                return false;
+            }
+
+            if(leftList == null)
+            {
+                leftList = new List<List<int>>();
+            }
+
+            leftList.Add(leftTemp);
+            return true;
+            //List<List<int>> list = new List<List<int>>();
+            //list.Add(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            //list.Add(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            //list.Add(new List<int> { 2, 3, 4, 5, 6, 7, 8, 10, 11, 13 });
+            //list.Add(new List<int> { 2, 5, 6, 7, 8, 9, 10, 13, 15, 17 });
+            //list.Add(new List<int> { 1, 2, 5, 6, 7, 8, 9, 13, 14, 15 });
+            //list.Add(new List<int> { 3, 4, 5, 6, 7, 9, 10, 11, 12, 13 });
+        }
+
+        /// <summary>
+        /// 获取选择行的数组
+        /// </summary>
+        /// <param name="rowIndex"></param>
+        /// <returns></returns>
+        private void SelectLeftList(int rowIndex)
+        {
+            if(rowIndex < 0 || rowIndex > leftList.Count)
+                return;
+            if (selectedList == null)
+                selectedList = new List<List<int>>();
+
+            var selectedItem = leftList[rowIndex];
+            if(!selectedList.Contains(selectedItem))
+            {
+                selectedList.Add(selectedItem);
+                lblSelected.Items.Add(lbxLeft.Items[rowIndex]);
+            }            
+        }
+
+        /// <summary>
+        /// 计算左侧的组合与选择的组合
+        /// </summary>
+        private void ListContains()
+        {
+            var leftListCopy = ObjectCopier.Clone(leftList);
+            //leftList.ForEach(i => leftListCopy.Add(i));
 
             //选择后的数据的组合
             List<int[]> selectedCombinationList = new List<int[]>();
-            foreach(var item in selectedList)
+            foreach (var item in selectedList)
             {
-                selectedCombinationList.AddRange(PermutationAndCombination<int>.GetCombination(item, 5));
+                selectedCombinationList.AddRange(PermutationAndCombination<int>.GetCombination(item.ToArray(), 5));
             }
 
             //判断左侧重复的组合
-            foreach (var item in leftList)
+            foreach (var leftItem in leftListCopy)
             {
-                var leftCombinations = PermutationAndCombination<int>.GetCombination(item, 5);
+                var leftCombinations = PermutationAndCombination<int>.GetCombination(leftItem.ToArray(), 5);
 
-                foreach (var lItem in leftCombinations)
+                foreach (var lcItem in leftCombinations) //左侧的组合
                 {
-                    foreach(var sItem in selectedCombinationList)
+                    if (selectedCombinationList.Any(s => s.All(lcItem.Contains)))
                     {
-                        if (sItem.All(lItem.Contains))
+                        foreach (var i in lcItem)   //选择的组合
                         {
-                            ArrayList arrayList = new ArrayList(item);
-                            arrayList.Remove()
+                            leftItem.Remove(i);
                         }
                     }
                 }
             }
-        }
-        
-        private List<List<int>> BuildLeftList()
-        {
-            List<List<int>> list = new List<List<int>>();
-            list.Add(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
-            list.Add(new List<int> { 2, 3, 4, 5, 6, 7, 8, 10, 11, 13 });
-            list.Add(new List<int> { 2, 5, 6, 7, 8, 9, 10, 13, 15, 17 });
-            list.Add(new List<int> { 1, 2, 5, 6, 7, 8, 9, 13, 14, 15 });
-            list.Add(new List<int> { 3, 4, 5, 6, 7, 9, 10, 11, 12, 13 });
-            return list;
+
+            OuputResult(leftListCopy);
         }
 
-        private int[] SelectLeftList(int rowIndex)
+        private void OuputResult(List<List<int>> result)
         {
-            if(rowIndex < 0 || rowIndex > leftList.Count)
-                return new int[]{};
-            return leftList[rowIndex];
+            List<int> temp = new List<int>();
+            foreach(var item in result)
+            {
+                if(item.Count > 0)
+                {
+                    temp.AddRange(item);
+                }
+            }
+
+            var strPers = temp.Distinct().OrderBy(o => o).Select(s => s.ToString().PadLeft(2, '0'));
+            var index = 0;
+            var pa = strPers.Skip(index++ * 5).Take(5);
+            while (pa.Count() > 0)
+            {
+                lbxOutput.Items.Add(string.Join(" ", pa));
+                pa = strPers.Skip(index++ * 5).Take(5);
+            }
+        }
+
+        private void btnClearA_Click(object sender, EventArgs e)
+        {
+            selectedList.Clear();
+            lblSelected.Items.Clear();
+        }
+
+        private void btnClearB_Click(object sender, EventArgs e)
+        {
+            lbxOutput.Items.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(selectedList == null || selectedList.Count == 0)
+            {
+                MessageBox.Show("先选择左侧的数据后再输出");
+                return;
+            }
+            ListContains();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtNums.Text))
+            {
+                MessageBox.Show("先输入数字");
+            }
+
+            if(BuildLeftList(txtNums.Text))
+            {
+                lbxLeft.Items.Add(txtNums.Text);
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog file = new OpenFileDialog())
+            {
+                file.Filter = "文本文件|*.txt";
+                if(file.ShowDialog() == DialogResult.OK)
+                {
+                    var allLines = File.ReadAllText(file.FileName);
+                    var lines = allLines.Replace("\r", "").Split('\n');
+                    foreach(var line in lines)
+                    {
+                        if(!string.IsNullOrEmpty(line))
+                        {
+                            if (BuildLeftList(line))
+                                lbxLeft.Items.Add(line);
+                            else
+                                break;
+                        }                        
+                    }
+                }
+            }
+        }
+
+        private void lbxLeft_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectLeftList(lbxLeft.SelectedIndex);
         }
     }
 }
